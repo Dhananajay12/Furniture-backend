5d9c5088502c354e45ce2294ed94bcf14e6daa74
Dhananajay12 <dhananjayavare786@gmail.com>
2024-01-02 00:25:50 +0530

    added model structure and controller

---
+++ src/model/Furniture.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+
+const furnitureSchema = new mongoose.Schema({
+
+	name: {
+		type: String,
+		require: true
+	},
+	image: {
+		type: String,
+	},
+	description: {
+		type: String,
+	},
+	price: {
+		type: Number,
+		default: 0
+	},
+	salePrice: {
+		type: Number,
+		default: 0
+	}
+})
+
+const Furniture = mongoose.model('furniure', furnitureSchema);
