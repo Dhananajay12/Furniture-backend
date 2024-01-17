dd8f01ef1125a7f9b856e0619a5529c25cdf1c7b
Dhananajay12 <dhananjayavare786@gmail.com>
2024-01-17 17:38:51 +0530

    added mongoDBURL

--- connection.ts
+++ connection.ts
@@ -1,18 +1,20 @@
 
 import mongoose from 'mongoose';
-import { authencatorMiddleware } from './src/middleware/Authenticator';
-// authencatorMiddleware();
 
 function connections(): void {
+	const mongoDBUrl = process.env.MONGO_DB_URL;
 
-	mongoose.connect("mongodb+srv://dhananjay:dhananjay@cluster0.rqk9e.mongodb.net/furniture?retryWrites=true&w=majority")
+	if (!mongoDBUrl) {
+		console.error("MongoDB URL is not defined in the environment variables.");
+		return;
+	}
+
+	mongoose.connect(mongoDBUrl)
 		.then(() => console.log("Succesfully conntected"))
-		.catch((err : Error) => console.log(err))
+		.catch((err: Error) => console.log(err))
 }
 
 
-// module.exports.readDB = mongoose.connection;
-// module.exports.writeDB = mongoose.connection;
 export { connections };
 export const readDB = mongoose.connection
 export const writeDB = mongoose.connection
