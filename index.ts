e48d6c0d3527a831e4a48af4988147fe8db97e03
Dhananajay12 <dhananjayavare786@gmail.com>
2024-05-01 21:22:13 +0530

    change in vercel

--- index.ts
+++ index.ts
@@ -12,7 +12,9 @@ dotenv.config()
 
 const app: Express = express();
 const server = http.createServer(app)
-app.use(cors());
+app.use(cors({
+	origin: "*",
+}));
 
 app.use(express.json())
 app.use('/api/v3', router)
